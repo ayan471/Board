c82e0327f73191bf930fddb6af27334937873cd4
Ayan Pakhira <pakhiraayan@gmail.com>
2024-04-05 21:27:22 +0530

    creating selection box

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,41 @@
+import { api } from "@/convex/_generated/api";
+import { auth, currentUser } from "@clerk/nextjs";
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const liveblocks = new Liveblocks({
+  secret:
+    "sk_dev_758i9aUUS0cKlf_dDD1Q3hfrw4nBOubhyqmfg35_1_XtWYurPPxe0kGO5S6EkyTl",
+});
+export async function POST(request: Request) {
+  const authorization = await auth();
+  const user = await currentUser();
+
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, { id: room });
+
+  if (board?.orgId !== authorization.orgId) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const userInfo = {
+    name: user.firstName || "Teammate",
+    picture: user.imageUrl,
+  };
+
+  const session = liveblocks.prepareSession(user.id, { userInfo });
+
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
+
+  const { status, body } = await session.authorize();
+
+  return new Response(body, { status });
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://viable-marlin-23.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
